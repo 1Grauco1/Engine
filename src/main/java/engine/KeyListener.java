bc0decdc4554178d1c8f259772a4bcbffaffeaf8
Grauco <g.ant.facul@gmail.com>
2025-04-15 10:50:34 -0300

    Commented

--- src/main/java/engine/KeyListener.java
+++ src/main/java/engine/KeyListener.java
@@ -7,8 +7,10 @@ public class KeyListener {
     private static KeyListener instance;
     private static boolean[] keyPressed = new boolean[350];
 
+    // Private constructor for singleton
     private KeyListener(){}
 
+    // Returns the single instance of KeyListener
     public static KeyListener get(){
         if(instance == null) {
             instance = new KeyListener();
@@ -16,14 +18,16 @@ public class KeyListener {
         return instance;
     }
 
+    // Called whenever a key is pressed or released
     public static void keyCallback(long window, int key, int scancode, int action, int mods){
         if(action == GLFW_PRESS){
-            get().keyPressed[key]= true;
+            get().keyPressed[key] = true;
         } else if(action == GLFW_RELEASE){
-            get().keyPressed[key]= false;
+            get().keyPressed[key] = false;
         }
     }
 
+    // Checks if a specific key is currently pressed
     public static boolean isKeyPressed(int keyCode) {
         return get().keyPressed[keyCode];
     }
--- src/main/java/engine/MouseListener.java
+++ src/main/java/engine/MouseListener.java
@@ -5,27 +5,34 @@ import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
 public class MouseListener {
     private static MouseListener instance;
+
     private double scrollX, scrollY;
-    private double xPos, yPos, lastX, lastY;
+    private double xPos, yPos;
+    private double lastX, lastY;
+
     private final boolean[] mouseButtonPressed = new boolean[3];
+
     private boolean isDragging;
 
+    // Private constructor (singleton)
     private MouseListener(){
-        this.scrollX= 0.0;
-        this.scrollY= 0.0;
-        this.xPos= 0.0;
-        this.yPos= 0.0;
-        this.lastX= 0.0;
-        this.lastY= 0.0;
+        this.scrollX = 0.0;
+        this.scrollY = 0.0;
+        this.xPos = 0.0;
+        this.yPos = 0.0;
+        this.lastX = 0.0;
+        this.lastY = 0.0;
     }
 
+    // Returns the only instance of MouseListener
     public static MouseListener get(){
         if(instance == null){
-            instance= new MouseListener();
+            instance = new MouseListener();
         }
         return instance;
     }
 
+    // Called when mouse moves
     public static void mousePosCallback(long window, double xpos, double ypos) {
         get().lastX = get().xPos;
         get().lastY = get().yPos;
@@ -34,6 +41,7 @@ public class MouseListener {
         get().isDragging = get().mouseButtonPressed[0] || get().mouseButtonPressed[1] || get().mouseButtonPressed[2];
     }
 
+    // Called when mouse button is pressed or released
     public static void mouseButtonCallback(long window, int button, int action, int mods){
         if(action == GLFW_PRESS) {
             if(button < get().mouseButtonPressed.length){
@@ -41,55 +49,67 @@ public class MouseListener {
             }
         } else if (action == GLFW_RELEASE) {
             if(button < get().mouseButtonPressed.length){
-                get().mouseButtonPressed[button]= false;
-                get().isDragging= false;
+                get().mouseButtonPressed[button] = false;
+                get().isDragging = false;
             }
-
         }
     }
 
-    public static void mouseScrollCallback(long window, double xOffset, double  yOffset){
-        get().scrollX= xOffset;
-        get().scrollY= yOffset;
+    // Called when mouse scrolls
+    public static void mouseScrollCallback(long window, double xOffset, double yOffset){
+        get().scrollX = xOffset;
+        get().scrollY = yOffset;
     }
 
+    // Called at the end of each frame
     public static void endFrame(){
-        get().scrollX= 0;
-        get().scrollY= 0;
-        get().lastX= get().xPos;
-        get().lastY= get().yPos;
+        get().scrollX = 0;
+        get().scrollY = 0;
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
     }
 
+    // Get current mouse X position
     public static float getX(){
         return (float) get().xPos;
     }
+
+    // Get current mouse Y position
     public static float getY(){
         return (float) get().yPos;
     }
 
+    // Get change in X since last frame
     public static float getDx(){
         return (float)(get().lastX - get().xPos);
     }
+
+    // Get change in Y since last frame
     public static float getDy(){
         return (float)(get().lastY - get().yPos);
     }
 
+    // Get scroll X movement
     public static float getScrollX(){
         return (float) get().scrollX;
     }
+
+    // Get scroll Y movement
     public static float getScrollY(){
         return (float) get().scrollY;
     }
 
+    // Check if mouse is dragging
     public static boolean isDragging(){
         return get().isDragging;
     }
 
+    // Check if a mouse button is pressed
     public static boolean mouseButtonDown(int button){
         if(button < get().mouseButtonPressed.length){
             return get().mouseButtonPressed[button];
+        } else {
+            return false;
         }
-        else{ return false; }
     }
-
 }
