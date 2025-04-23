fd23c65d2bcdda84bfa1d0c5ab3a2bbdaa34d90e
Grauco <g.ant.facul@gmail.com>
2025-04-23 12:03:35 -0300

    Components Update

--- src/main/java/components/SpriteRender.java
+++ src/main/java/components/SpriteRender.java
@@ -1,10 +1,26 @@
 package components;
 
 import engine.Component;
+import org.joml.Vector4f;
 
 public class SpriteRender extends Component {
+
+    private Vector4f color;
+
+    public SpriteRender(Vector4f color){
+        this.color= color;
+    }
+
+    public Vector4f getColor() {
+        return this.color;
+    }
+
+    @Override
+    public void start(){
+
+    }
     @Override
     public void update(float dt) {
-        System.out.println("Im updating");
+
     }
 }
